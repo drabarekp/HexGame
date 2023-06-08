78eaa4e12751b70a90606e8732c6c09d6c3ef0d7
Jakub Rymuza <jakubrymuza@gmail.com>
2023-06-08 13:53:35 +0200

    game state changes

--- HexGame/Models/GameState.cs
+++ HexGame/Models/GameState.cs
@@ -1,53 +1,53 @@
 ﻿using HexGame.Enums;
-using HexGame.Exceptions;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Configuration.Internal;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
 
 namespace HexGame.Models
 {
-    internal class GameState
+    internal class GameState : ICloneable
     {
-        const int SIZE = 11;
+        const int Size = 11;
         public HexStateEnum[][] Board { get; init; }
         public HexStateEnum CurrentMove { get; private set; }
+        public GameMove LastMove { get; private set; }
 
         public GameState()
         {
-            Board = new HexStateEnum[SIZE][];
-            for(int i = 0; i< SIZE; i++)
+            Board = new HexStateEnum[Size][];
+            for (int i = 0; i < Size; i++)
             {
-                Board[i] = new HexStateEnum[SIZE];
+                Board[i] = new HexStateEnum[Size];
             }
 
             InitializeBoard();
             CurrentMove = HexStateEnum.Red;
         }
 
-        private void InitializeBoard()
+        public object Clone()
         {
-            for (int i = 0; i < SIZE; i++)
-            {
-                for (int j = 0; j < SIZE; j++)
-                {
-                    Board[i][j] = HexStateEnum.None;
-                }
-            }
+            throw new NotImplementedException();
         }
 
-        public void PlayerMove(int row, int column)
-        {
-            if (Board[row][column] != HexStateEnum.None) return;
+        public double GetScore() => throw new NotImplementedException();
 
-            Board[row][column] = CurrentMove;
-            
-            if(CurrentMove == HexStateEnum.Red)
+        public GameState GetNextState(GameMove move)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<GameMove> GetPossibleMoves()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PerformMove(GameMove move)
+        {
+            if (Board[move.Row][move.Column] != HexStateEnum.None) return;
+
+            Board[move.Row][move.Column] = CurrentMove;
+
+            if (CurrentMove == HexStateEnum.Red)
             {
                 CurrentMove = HexStateEnum.Blue;
             }
@@ -57,34 +57,30 @@ namespace HexGame.Models
             }
         }
 
-        private HexStateEnum GetColor(GameField field)
+        public static GameField[] GetNeighbours(int row, int column)
         {
-            return Board[field.Row][field.Column];
-        }
+            var neighbours = new List<GameField>
+            {
+                new GameField(row - 1, column),
+                new GameField(row - 1, column + 1),
+                new GameField(row, column - 1),
+                new GameField(row, column + 1),
+                new GameField(row + 1, column - 1),
+                new GameField(row + 1, column)
+            };
 
-        public GameField[] GetNeighbours(int row, int column)
-        {
-            List<GameField> neighbours = new List<GameField>();
-
-            neighbours.Add(new GameField(row - 1, column));
-            neighbours.Add(new GameField(row - 1, column + 1));
-            neighbours.Add(new GameField(row, column - 1));
-            neighbours.Add(new GameField(row, column + 1));
-            neighbours.Add(new GameField(row + 1, column - 1));
-            neighbours.Add(new GameField(row + 1, column));
-
-            return neighbours.Where(n => n.Column >= 0 && n.Column < SIZE && n.Row >= 0 && n.Row < SIZE).ToArray();
+            return neighbours.Where(n => n.Column >= 0 && n.Column < Size && n.Row >= 0 && n.Row < Size).ToArray();
         }
 
         public GameResultEnum GetGameResult()
         {
             {
                 // checking red victory
-                bool[][] redVisited = new bool[SIZE][];
-                for (int i = 0; i < SIZE; i++) redVisited[i] = new bool[SIZE];
+                bool[][] redVisited = new bool[Size][];
+                for (int i = 0; i < Size; i++) redVisited[i] = new bool[Size];
 
                 var redStack = new Stack<GameField>();
-                for (int i = 0; i < SIZE; i++)
+                for (int i = 0; i < Size; i++)
                 {
                     if (Board[0][i] == HexStateEnum.Red)
                     {
@@ -102,7 +98,7 @@ namespace HexGame.Models
                         // if red and not visited yet
                         if (GetColor(neighbouringField) == HexStateEnum.Red && !redVisited[neighbouringField.Row][neighbouringField.Column])
                         {
-                            if (neighbouringField.Row == SIZE - 1) return GameResultEnum.RedVictory;
+                            if (neighbouringField.Row == Size - 1) return GameResultEnum.RedVictory;
 
                             redStack.Push(neighbouringField);
                             redVisited[neighbouringField.Row][neighbouringField.Column] = true;
@@ -112,11 +108,11 @@ namespace HexGame.Models
             }
             {
                 // checking blue victory
-                bool[][] blueVisited = new bool[SIZE][];
-                for (int i = 0; i < SIZE; i++) blueVisited[i] = new bool[SIZE];
+                bool[][] blueVisited = new bool[Size][];
+                for (int i = 0; i < Size; i++) blueVisited[i] = new bool[Size];
 
                 var blueStack = new Stack<GameField>();
-                for (int i = 0; i < SIZE; i++)
+                for (int i = 0; i < Size; i++)
                 {
                     if (Board[i][0] == HexStateEnum.Blue)
                     {
@@ -134,7 +130,7 @@ namespace HexGame.Models
                         // if blue and not visited yet
                         if (GetColor(neighbouringField) == HexStateEnum.Blue && !blueVisited[neighbouringField.Row][neighbouringField.Column])
                         {
-                            if (neighbouringField.Column == SIZE - 1) return GameResultEnum.BlueVictory;
+                            if (neighbouringField.Column == Size - 1) return GameResultEnum.BlueVictory;
 
                             blueStack.Push(neighbouringField);
                             blueVisited[neighbouringField.Row][neighbouringField.Column] = true;
@@ -144,6 +140,24 @@ namespace HexGame.Models
             }
 
             return GameResultEnum.InconclusiveYet;
+        }
+
+        public bool IsTerminal() => GetGameResult() != GameResultEnum.InconclusiveYet;
+
+        private HexStateEnum GetColor(GameField field)
+        {
+            return Board[field.Row][field.Column];
+        }
+
+        private void InitializeBoard()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Board[i][j] = HexStateEnum.None;
+                }
+            }
         }
     }
 }
