8e14b437d53125c1b5848ea853aa701b36370b8a
Jakub Rymuza <jakubrymuza@gmail.com>
2023-06-09 12:20:59 +0200

    implement simulation runner

--- HexGame/GameServices/SimulationRunner.cs
+++ HexGame/GameServices/SimulationRunner.cs
@@ -1,14 +1,15 @@
 ﻿using HexGame.Engine;
 using HexGame.Enums;
-using System;
+using HexGame.Models;
+using System.Threading.Tasks;
 
 namespace HexGame.GameServices
 {
     internal class SimulationRunner
     {
-        private IAlgorithm Algorithm1;
-        private IAlgorithm Algorithm2;
-        private int Repetitions;
+        private readonly IAlgorithm Algorithm1;
+        private readonly IAlgorithm Algorithm2;
+        private readonly int Repetitions;
 
         public SimulationRunner(IAlgorithm algorithm1, IAlgorithm algorithm2, int repetitions)
         {
@@ -17,18 +18,72 @@ namespace HexGame.GameServices
             Repetitions = repetitions;
         }
 
-        public int RunSimulations()
+        public int RunSimulations(int seed)
         {
             int wins = 0;
+            var winsLock = new object();
 
-            // TODO
+            Parallel.For(0, Repetitions, i =>
+            {
+                var playerStarting = (PlayerEnum)(i % 2);
+                var newAlgorithm1 = Algorithm1.Copy(i * seed);
+                var newAlgorithm2 = Algorithm2.Copy(i * seed);
+                var gameState = new GameState();
+
+                var result = RunSimulation(gameState, newAlgorithm1, newAlgorithm2, playerStarting);
+
+                if (playerStarting == PlayerEnum.Red && result == GameResultEnum.RedVictory ||
+                    playerStarting == PlayerEnum.Blue && result == GameResultEnum.BlueVictory)
+                {
+                    lock (winsLock)
+                    {
+                        wins++;
+                    }
+                }
+
+            });
 
             return wins;
         }
 
-        private GameResultEnum RunSimulation()
+        private static GameResultEnum RunSimulation(GameState gameState, IAlgorithm algorithm1, IAlgorithm algorithm2, PlayerEnum playerStarting)
         {
-            throw new NotImplementedException();
+            GameResultEnum result;
+
+            if (playerStarting == PlayerEnum.Red)
+            {
+                while (true)
+                {
+                    result = gameState.GetGameResult();
+
+                    if (result != GameResultEnum.InconclusiveYet) break;
+
+                    var move1 = algorithm1.CalculateNextMove(gameState, playerStarting);
+                    gameState.PerformMove(move1);
+
+                    var move2 = algorithm2.CalculateNextMove(gameState, playerStarting);
+                    gameState.PerformMove(move2);
+                }
+            }
+            else
+            {
+                while (true)
+                {
+                    result = gameState.GetGameResult();
+
+                    if (result != GameResultEnum.InconclusiveYet) break;
+
+                    var move1 = algorithm2.CalculateNextMove(gameState, playerStarting);
+                    gameState.PerformMove(move1);
+
+                    var move2 = algorithm1.CalculateNextMove(gameState, playerStarting);
+                    gameState.PerformMove(move2);
+
+                }
+
+            }
+
+            return result;
         }
 
 
