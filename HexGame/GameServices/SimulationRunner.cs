ce0955cc009f47b9d1e0a408ac43d2caf78df028
Jakub Rymuza <jakubrymuza@gmail.com>
2023-06-09 11:36:28 +0200

    add simulation runner

---
+++ HexGame/GameServices/SimulationRunner.cs
@@ -0,0 +1,37 @@
+﻿using HexGame.Engine;
+using HexGame.Enums;
+using System;
+
+namespace HexGame.GameServices
+{
+    internal class SimulationRunner
+    {
+        private IAlgorithm Algorithm1;
+        private IAlgorithm Algorithm2;
+        private int Repetitions;
+
+        public SimulationRunner(IAlgorithm algorithm1, IAlgorithm algorithm2, int repetitions)
+        {
+            Algorithm1 = algorithm1;
+            Algorithm2 = algorithm2;
+            Repetitions = repetitions;
+        }
+
+        public int RunSimulations()
+        {
+            int wins = 0;
+
+            // TODO
+
+            return wins;
+        }
+
+        private GameResultEnum RunSimulation()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
--- HexGame/MainWindow.xaml.cs
+++ HexGame/MainWindow.xaml.cs
@@ -1,12 +1,13 @@
-﻿using HexGame.GameServices;
+﻿using HexGame.Engine;
+using HexGame.Enums;
+using HexGame.GameServices;
+using System;
 using System.Drawing;
 using System.IO;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
-using HexGame.Enums;
-using System;
-using System.Threading;
 using System.Windows.Threading;
 
 namespace HexGame
@@ -35,16 +36,11 @@ namespace HexGame
             topGameService.NewGame(popup.Iterations, popup.IsPlayerStart, popup.AlgorithmType, random.Next());
             UpdateBoard();
 
-            if(!popup.IsPlayerStart) 
+            if (!popup.IsPlayerStart)
             {
                 topGameService.PerformAIMove();
                 UpdateBoard();
             }
-        }
-
-        private void newTournament_Clicked(object sender, MouseButtonEventArgs e)
-        {
-            // TODO
         }
 
         private void UpdateBoard()
@@ -87,22 +83,86 @@ namespace HexGame
             ShowAIProcessing();
             Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, (ThreadStart)(() =>
             {
-                
                 topGameService.PerformAIMove();
                 UpdateBoard();
                 HideAIProcessing();
-            }));   
+            }));
         }
 
         private void ShowAIProcessing()
         {
             AIStatusBar.Text = "AI myśli, proszę czekać.";
-            
+
         }
 
         private void HideAIProcessing()
         {
             AIStatusBar.Text = "Program gotowy";
+        }
+
+        private void StartSimulationButton_Click(object sender, RoutedEventArgs e)
+        {
+            int iterations = int.Parse(IterationsTextBox.Text);
+            int repetitions = int.Parse(RepetitionsTextBox.Text);
+            int seed = int.Parse(SeedTextBox.Text);
+
+            IAlgorithm algorithm1 = SetAlgorithm(Algorithm1TypeComboBox.SelectedIndex, iterations, seed);
+            IAlgorithm algorithm2 = SetAlgorithm(Algorithm2TypeComboBox.SelectedIndex, iterations, seed);
+
+            ResetResults();
+
+            ShowAIProcessing();
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, (ThreadStart)(() =>
+            {
+                var simulationRunner = new SimulationRunner(algorithm1, algorithm2, repetitions);
+                int algorithm1Wins = simulationRunner.RunSimulations();
+
+                UpdateResults(algorithm1.AlgorithmName, algorithm2.AlgorithmName, algorithm1Wins, repetitions);
+
+                HideAIProcessing();
+            }));
+        }
+
+        private void ResetResults()
+        {
+            Algorithm1Label.Content = "Symulacja w toku";
+            Algorithm1WinsLabel.Content = "";
+            Algorithm1DefeatsLabel.Content = "";
+
+            Algorithm2Label.Content = "";
+            Algorithm2WinsLabel.Content = "";
+            Algorithm2DefeatsLabel.Content = "";
+        }
+
+        private void UpdateResults(string algorithm1Name, string algorithm2Name, int algorithm1Wins, int repetitions)
+        {
+            Algorithm1Label.Content = algorithm1Name;
+            Algorithm1WinsLabel.Content = algorithm1Wins;
+            Algorithm1DefeatsLabel.Content = repetitions - algorithm1Wins;
+
+            Algorithm2Label.Content = algorithm2Name;
+            Algorithm2WinsLabel.Content = repetitions - algorithm1Wins;
+            Algorithm2DefeatsLabel.Content = algorithm1Wins;
+        }
+
+        private IAlgorithm SetAlgorithm(int index, int iterations, int seed)
+        {
+            switch (index)
+            {
+                case (int)AlgorithmTypeEnum.BasicMCTS:
+                    return new BasicMCTSAlgorithm(seed, iterations);
+
+                case (int)AlgorithmTypeEnum.RAVE:
+                    return new BasicMCTSAlgorithm(seed, iterations);
+
+                case (int)AlgorithmTypeEnum.MAST:
+                    return new BasicMCTSAlgorithm(seed, iterations);
+
+                case (int)AlgorithmTypeEnum.Heuristic:
+                    return new BasicMCTSAlgorithm(seed, iterations);
+            }
+
+            throw new InvalidOperationException();
         }
     }
 }
