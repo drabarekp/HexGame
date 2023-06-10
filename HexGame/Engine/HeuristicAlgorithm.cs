4ba86aac54945019a6dcc65bfc840cfb5066b34f
Paweł Drabarek <01151568@pw.edu.pl>
2023-06-10 23:19:35 +0200

    heuristic 2.0

--- HexGame/Engine/HeuristicAlgorithm.cs
+++ HexGame/Engine/HeuristicAlgorithm.cs
@@ -2,6 +2,9 @@
 using HexGame.Enums;
 using HexGame.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
 using System.Xml.Linq;
 
 namespace HexGame.Engine
@@ -17,47 +20,96 @@ namespace HexGame.Engine
 
         public override GameMove CalculateNextMove(GameState state, PlayerEnum player)
         {
-            var root = CreateRoot(state);
-            var children = root.State.GetPossibleMoves();
+            var root = new HeuristicNode((GameState)state.Clone());
 
-            double currentHeuristic = double.MinValue;
-            GameMove currentMove = new GameMove();
+            Queue<HeuristicNode> nodes = new Queue<HeuristicNode>();
+            nodes.Enqueue(root);
 
-            foreach(var c in children)
+            //creating move tree
+            for (int i = 0; i < Iterations; )
             {
-
-                var newState = state.GetNextState(c);
-                double heuristic = CalculateHeuristic(newState, player);
-
-                if(heuristic > currentHeuristic)
+                try
                 {
-                    currentHeuristic = heuristic;
-                    currentMove = c;
+                    var node = nodes.Dequeue();
+                
+                    foreach(var move in node.State.GetPossibleMoves())
+                    {
+                        HeuristicNode childNode = (HeuristicNode)node.AddChild(node.State.GetNextState(move));
+                        nodes.Enqueue(childNode);
+                        i++;
+                    }
                 }
-
+                catch (Exception) { break; }
             }
 
-            return currentMove;
+            MinMaxRecursively(root);
+
+            if(player == PlayerEnum.Red)
+            {
+                var maxChild = root.Children.MaxBy(n => ((HeuristicNode)n).Heuristic);
+                return maxChild.State.LastMove;
+            }
+            else if(player == PlayerEnum.Blue)
+            {
+                var minChild = root.Children.MinBy(n => ((HeuristicNode)n).Heuristic);
+                return minChild.State.LastMove;
+            }
+
+            throw new ArgumentException();
         }
 
-        private double CalculateHeuristic(GameState state, PlayerEnum player)
+        private void MinMaxRecursively(HeuristicNode root)
+        {
+            if(root.Children.Count == 0)
+            {
+                root.Heuristic = CalculateHeuristic(root.State);
+            }
+            else
+            {
+                if(root.State.CurrentMove == HexStateEnum.Red)
+                {
+                    foreach (var childNode in root.Children)
+                    {
+                        MinMaxRecursively((HeuristicNode)childNode);
+                    }
+                    double current = double.MinValue;
+                    foreach (HeuristicNode childNode in root.Children)
+                    {
+                        if(childNode.Heuristic > current)
+                        {
+                            current = childNode.Heuristic;
+                        }
+                    }
+                    root.Heuristic = current;
+                }
+                else if (root.State.CurrentMove == HexStateEnum.Blue)
+                {
+                    foreach (var childNode in root.Children)
+                    {
+                        MinMaxRecursively((HeuristicNode)childNode);
+                    }
+                    double current = double.MaxValue;
+                    foreach (HeuristicNode childNode in root.Children)
+                    {
+                        if (childNode.Heuristic < current)
+                        {
+                            current = childNode.Heuristic;
+                        }
+                    }
+                    root.Heuristic = current;
+                }
+                
+            }
+        }
+
+        private double CalculateHeuristic(GameState state)
         {
             HexNetworkGraph redGraph = new HexNetworkGraph(state, HexStateEnum.Red);
             HexNetworkGraph blueGraph = new HexNetworkGraph(state, HexStateEnum.Blue);
             double redLength = redGraph.FindShortestPath();
             double blueLength = blueGraph.FindShortestPath();
-            double heuristic = 0.0;
 
-            if (player == PlayerEnum.Red)
-            {
-                heuristic = blueLength - redLength;
-            }
-            else
-            {
-                heuristic = redLength - blueLength;
-            }
-
-            return heuristic;
+            return blueLength - redLength; // + red good, - blue good
         }
     }
 }
---
+++ HexGame/Engine/Nodes/HeuristicNode.cs
@@ -0,0 +1,31 @@
+﻿using HexGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexGame.Engine.Nodes
+{
+    internal class HeuristicNode : Node
+    {
+        public HeuristicNode(GameState state) : base(state)
+        {
+        }
+
+        public double Alpha { get; set; }
+        public double Beta { get; set; }
+        public double Heuristic { get; set; }
+
+        public override INode AddChild(GameState childState)
+        {
+            var childNode = new HeuristicNode(childState)
+            {
+                Parent = this
+            };
+
+            Children.Add(childNode);
+            return childNode;
+        }
+    }
+}
